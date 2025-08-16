545183b9d091d705ecfcc3acad4e8d7f9407ec32
Darel Masis <darrelmasis@gmail.com>
2025-08-16 16:09:16 -0600

    Elimina valores predeterminados en la configuración para utilizar únicamente variables de entorno en config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
 module.exports = {
-  port: process.env.PORT || 3000,
-  iconsPath: process.env.ICONS_CSV_PATH || './data/icons.csv',
-  defaultFill: process.env.DEFAULT_FILL || '#000000',
-  defaultVariant: process.env.DEFAULT_VARIANT || 'regular',
-  corsOrigin: process.env.CORS_ORIGIN || '*',
-  defaultSize: parseInt(process.env.DEFAULT_ICON_SIZE) || 128
+  port: process.env.PORT,
+  iconsPath: process.env.ICONS_CSV_PATH,
+  defaultFill: process.env.DEFAULT_FILL,
+  defaultVariant: process.env.DEFAULT_VARIANT,
+  corsOrigin: process.env.CORS_ORIGIN,
+  defaultSize: parseInt(process.env.DEFAULT_ICON_SIZE)
 };
